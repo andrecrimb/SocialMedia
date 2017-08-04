7e0b2cf59fb68a1e29b681b230f6888c98d4eb0e
Andre Rosa <andrerosa.tech@gmail.com>
2017-08-04 09:24:51 -0300

    athentication with facebook and firebase

--- SocialMedia/SignInVC.swift
+++ SocialMedia/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -20,6 +23,34 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookBtnPress(_ sender: UIButton) {
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil{
+                print("ANDRE: unable to authenticate - \(String(describing: error))")
+            } else if result?.isCancelled == true {
+                print("ANDRE: user cancel authentication")
+            } else{
+                
+                print("ANDRE: sucess to authenticate")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                
+                self.firebaseAuth(credential)
+                
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential){
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil{
+                print("ANDRE: unable to authenticate to firebase - \(String(describing: error))")
+            } else {
+                print("ANDRE: sucesss authenticate to firebase")
+            }
+        })
+    }
+    
 
 }
 
--- SocialMedia/TimelineVC.swift
+++ SocialMedia/TimelineVC.swift
@@ -8,30 +8,31 @@
 
 import UIKit
 
-class TimelineVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+//class TimelineVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class TimelineVC: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        tableView.delegate = self
-        tableView.dataSource = self
-        
+//        tableView.delegate = self
+//        tableView.dataSource = self
+//        
         
     }
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-    }
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        return 1
+//    }
+//    
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 10
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        
+//    }
     
 
 }
